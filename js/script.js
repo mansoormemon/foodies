11519c12c4ec4471f7e6f79eb4013f1d94e268c5
Mansoor Ahmed Memon <mansoorahmed.one@gmail.com>
2023-04-09 15:19:24 +0500

    Add validation to sign up page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+'use strict'
+
+
 $(document).ready(
     () => {
         $("#signUpAgreementChk").change(
@@ -5,5 +8,24 @@ $(document).ready(
                 $("#signUpSubmit").prop("disabled", !$("#signUpAgreementChk").prop("checked"));
             }
         );
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles.
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+
+        $(".form-check").css("color", "#0f0f0f");
+
     }
 );
