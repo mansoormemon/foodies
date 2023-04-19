26b116bf357aa2f3b9e4db7b3618ad0318debddc
coolfaiz <coolfaiz365@gmail.com>
2023-04-19 19:53:04 +0500

    First Commit

--- js/script.js
+++ js/script.js
@@ -1,37 +1,49 @@
-'use strict'
+// 'use strict'
 
 
-$(document).ready(
-    () => {
-        $("#signUpAgreementChk").change(
-            () => {
-                $("#signUpSubmit").prop("disabled", !$("#signUpAgreementChk").prop("checked"));
-            }
-        );
+// $(document).ready(
+//     () => {
+//         $("#signUpAgreementChk").change(
+//             () => {
+//                 $("#signUpSubmit").prop("disabled", !$("#signUpAgreementChk").prop("checked"));
+//             }
+//         );
 
-        $("#signUpShowPassword").click(
-            () => {
-                var signUpPassword = $("#signUpPassword");
-                signUpPassword.attr('type', signUpPassword.attr('type') == "password" ? "text" : "password");
-                $("#signUpEye").toggleClass('d-none');
-                $("#signUpEyeSlash").toggleClass('d-none');
-            }
-        );
+//         $("#signUpShowPassword").click(
+//             () => {
+//                 var signUpPassword = $("#signUpPassword");
+//                 signUpPassword.attr('type', signUpPassword.attr('type') == "password" ? "text" : "password");
+//                 $("#signUpEye").toggleClass('d-none');
+//                 $("#signUpEyeSlash").toggleClass('d-none');
+//             }
+//         );
 
-        // Fetch all the forms we want to apply custom Bootstrap validation styles.
-        var forms = document.querySelectorAll('.needs-validation')
+//         // Fetch all the forms we want to apply custom Bootstrap validation styles.
+//         var forms = document.querySelectorAll('.needs-validation')
 
-        // Loop over them and prevent submission
-        Array.prototype.slice.call(forms)
-            .forEach(function (form) {
-                form.addEventListener('submit', function (event) {
-                    if (!form.checkValidity()) {
-                        event.preventDefault()
-                        event.stopPropagation()
-                    }
+//         // Loop over them and prevent submission
+//         Array.prototype.slice.call(forms)
+//             .forEach(function (form) {
+//                 form.addEventListener('submit', function (event) {
+//                     if (!form.checkValidity()) {
+//                         event.preventDefault()
+//                         event.stopPropagation()
+//                     }
 
-                    form.classList.add('was-validated')
-                }, false)
-            });
+//                     form.classList.add('was-validated')
+//                 }, false)
+//             });
+//     }
+// );
+d1 = document.getElementsByClassName('hamburg')[0];
+d2 = document.getElementsByClassName("hammenu")[0];
+
+
+d1.addEventListener("click", function () {
+    if (d2.style.display == "flex") {
+        d2.style.display = "none"
     }
-);
+    else {
+        d2.style.display = "flex"
+    }
+})
