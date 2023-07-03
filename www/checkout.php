df9a8f0962839c80bef4754a08ccce6f5bc92b22
haroon10725 <haroon347@gmail.com>
2023-07-03 12:00:30 +0500

    Edited the checkout.php

--- www/checkout.php
+++ www/checkout.php
@@ -1,10 +1,47 @@
 <?php
-    session_start();
+session_start();
+include "dbh.php";
 
+$is_logged_in = isset($_SESSION["CUSTOMER_ID"]);
+
+if ($is_logged_in) {
+    $cust_id = $_SESSION['CUSTOMER_ID'];
+
+    // Get the customer details
+    $sql = "SELECT * FROM customer WHERE CUSTOMER_ID = $cust_id;";
+    $result = mysqli_query($conn, $sql);
+    $customer = mysqli_fetch_assoc($result);
+
+    // Get the cart items
+    $cart_items = $_SESSION["CART_ITEMS"];
+
+    // Insert data into salesorder table
+    $insert_order_query = "INSERT INTO salesorder (CUSTOMER_ID) VALUES ('$cust_id')";
+    mysqli_query($conn, $insert_order_query);
+    $sales_order_id = mysqli_insert_id($conn);
+
+    // Insert data into ordereditem table
+    foreach ($cart_items as $food_item_id => $quantity) {
+        $insert_item_query = "INSERT INTO ordereditem (SALES_ORDER_ID, FOOD_ITEM_ID, QUANTITY) VALUES ('$sales_order_id', '$food_item_id', '$quantity')";
+        mysqli_query($conn, $insert_item_query);
+    }
+
+    // Retrieve payment method ID from the form submission
     $payment_method = $_POST["paymentMethod"];
-    $cart = $_SESSION["CART_ITEMS"];
+    if ($payment_method=='Card'){
+        $payment_method=2;
+    }else{
+        $payment_method=1;
+    }
 
+    // Insert data into payment table
+    $insert_payment_query = "INSERT INTO payment (SALES_ORDER_ID, PAYMENT_METHOD_ID) VALUES ('$sales_order_id', '$payment_method')";
+    mysqli_query($conn, $insert_payment_query);
+
+    // Clear the cart
     $_SESSION["CART_ITEMS"] = [];
 
+    // Redirect to success page or display a success message
     header("Location: ../index.php");
+} 
 ?>
--- www/sign_up_inc.php
+++ www/sign_up_inc.php
@@ -14,16 +14,20 @@
         header("Location: sign_up.php?e=0");
     }
     else {
-        $sql = "INSERT INTO customer (EMAIL, PASSCODE, CONTACT) VALUES ('$email', '$password', '$contact');";
-
+        $sql = "INSERT INTO customer (EMAIL, PASSCODE) VALUES ('$email', '$password');";
+        
         if (mysqli_query($conn, $sql)){
             $sql_get_cust_id = "SELECT CUSTOMER_ID FROM customer WHERE EMAIL = \"$email\";";
             $result = mysqli_query($conn, $sql_get_cust_id);
             $record = mysqli_fetch_assoc($result);
             $cust_id = $record["CUSTOMER_ID"];
+            $sql1 = "INSERT INTO customerphone (CUSTOMER_ID, PHONE_NO) VALUES ('$cust_id', '$contact');";
+            $result1 = mysqli_query($conn, $sql1);
             $_SESSION["CUSTOMER_ID"] = $cust_id;
             
             header("Location: ../index.php");
         }
     }
 ?>
+
+
